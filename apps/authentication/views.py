712cb4bd00eb01bff290596818434f724487aeec
Husan <102412893+hwandev12@users.noreply.github.com>
2022-08-08 12:14:56 +0500

    make login authen

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, reverse, redirect
 from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # create sign up view
 def signup(request):
@@ -8,5 +10,23 @@ def signup(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("/")
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account was created for " + user)
+            return redirect("login")
     return render(request, 'registration/signup.html', {"form": form})
+
+
+def loginView(request):
+
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return reverse("blog:home")
+
+    return render(request, 'registration/login.html')
--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -1,7 +1,7 @@
 from random import betavariate
 from django.urls import path, include
 from . import views
-from apps.authentication.views import signup
+from apps.authentication.views import signup, loginView
 
 app_name = 'blog'
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('signup/', signup, name='signup'),
+    path('login/', loginView, name='login')
 ]
