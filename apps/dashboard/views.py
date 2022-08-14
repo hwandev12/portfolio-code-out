d95778108f4e65b4dccc57e12e09ae2ef263a6d1
Husan <102412893+hwandev12@users.noreply.github.com>
2022-08-14 11:35:08 +0500

    add how many complaints and offers to dashboard page

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -11,11 +11,15 @@ def dashboard(request):
 	contact = Contact.objects.count()
 	contact_objects = Contact.objects.all()
 	all_users = Users.objects.all()
+	complaints = Contact.objects.filter(category__name='Shikoyat').count()
+	offers = Contact.objects.filter(category__name='Taklif').count()
 	context = {
 		"users": users,
 	 	"posts": posts,
 	  	"contact": contact,
 	  	"all_users": all_users,
-	  	"contact_objects": contact_objects
+	  	"contact_objects": contact_objects,
+	  	"complaints": complaints,
+	  	"offers": offers
 	}
 	return render(request, 'dashboard/pages/dashboard.html', context)
