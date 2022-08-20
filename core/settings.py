437433c84c1756b509bb6ebd2e8e7dffb2caeb03
hwandev12 <husan.ahmedov777@yandex.com>
2022-08-20 17:52:41 +0300

    add beta version

--- core/settings.py
+++ core/settings.py
@@ -74,6 +74,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'digest blog',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
